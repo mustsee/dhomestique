5b7034f955241d718f3ee8c2bc3d979fe0249373
Thomas <thomas.sypniewski@gmail.com>
2019-06-07 13:16:32 +0200

    change header font-size on small mobile screen (width < 374px)

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,44 +1,49 @@
 <template>
   <v-app>
-  <v-toolbar app flat height="80" color="#fff">
-    <v-toolbar-title class="title">d<span class="home">home</span>stique</v-toolbar-title>
-    <v-spacer></v-spacer>
-    <v-icon color="#FF8C00">local_phone</v-icon>
-    <span class="phone-number">06 58 86 95 90</span>
-  </v-toolbar>
+    <v-toolbar app flat height="80" color="#fff">
+      <v-toolbar-title class="title">
+        d<span class="home">home</span>stique
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-icon color="#FF8C00">local_phone</v-icon>
+      <span class="phone-number">06 58 86 95 90</span>
+    </v-toolbar>
     <v-content>
       <v-container fluid>
-        <nuxt />
+        <nuxt/>
       </v-container>
     </v-content>
-    <v-footer
-      class="justify-center"
-      color="#fff"
-    >
+    <v-footer class="justify-center" color="#fff">
       <span>&copy; 2019</span>
     </v-footer>
   </v-app>
 </template>
 
 <style lang="scss" scoped>
-  .container {
-    padding: 0; 
-    min-width: 300px;
+.container {
+  padding: 0;
+  min-width: 300px;
+}
+.title {
+  text-transform: uppercase;
+  font-size: 24px !important;
+  .home {
+    color: #ff8c00;
   }
-  .title {
-    text-transform: uppercase;
-    font-size: 24px !important;
-    .home {
-      color: #FF8C00;
-    }
+  @media (max-width: 374px) {
+    font-size: 20px !important;
   }
-  .phone-number {
-    font-size: 20px;
-    margin-left: 5px;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-    overflow: hidden;
-    width:auto;
+}
+.phone-number {
+  font-size: 20px;
+  margin-left: 5px;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
+  width: auto;
+  @media (max-width: 374px) {
+    font-size: 16px;
   }
+}
 </style>
 
