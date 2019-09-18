e5c65f1c018ecbd0fb5a065d1df18e344a50543a
Thomas <thomas.sypniewski@gmail.com>
2019-09-18 22:46:04 +0200

    add google analytics

--- nuxt.config.js
+++ nuxt.config.js
@@ -48,7 +48,8 @@ export default {
   ** Plugins to load before mounting the App
   */
   plugins: [
-    '@/plugins/vuetify'
+    '@/plugins/vuetify',
+    { src: '~plugins/ga.js', ssr: false }
   ],
 
   /*
